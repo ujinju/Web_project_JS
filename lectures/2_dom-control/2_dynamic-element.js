04fcaeaa584b33b1575848f62a20165e3c9205cc
ujinju <jinju201118@gmail.com>
2023-04-13 20:35:33 +0900

    Document

--- lectures/2_dom-control/2_dynamic-element.js
+++ lectures/2_dom-control/2_dynamic-element.js
@@ -1,13 +1,25 @@
 let addBtn = document.getElementById('button');
 let addBeforeBtn = document.getElementById('before');
 let inputBox = document.querySelector('input');
+let removeTargetBtn = document.querySelector('#target-remove');
+
+let removeBtn = document.querySelector('.remove-btn');
 
 addBtn.addEventListener('click', function () {
   // alert('hello');
   let li = document.createElement('li');
   let ul = document.querySelector('ul');
   li.textContent = inputBox.value;
+
+  let button = document.createElement('button');
+  button.textContent = 'X';
+  // button.setAttribute('class', 'remove-btn');
+  button.classList.add('remove-btn');
+  button.addEventListener('click', removeParentNode);
+  li.appendChild(button);
   ul.appendChild(li);
+  inputBox.value = '';
+  inputBox.focus();
 });
 
 addBeforeBtn.addEventListener('click', function () {
@@ -19,3 +31,14 @@ addBeforeBtn.addEventListener('click', function () {
   // ul.appendChild(li);
   ul.insertBefore(li, targetLi);
 });
+
+removeTargetBtn.addEventListener('click', function () {
+  let targetLi = document.querySelector('li#target');
+  targetLi.remove();
+});
+
+removeBtn.addEventListener('click', removeParentNode);
+
+function removeParentNode(event) {
+  event.target.parentNode.remove();
+}
