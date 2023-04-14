5e1ca910a9d49aafea6738e07261a1fbd0d4378d
ujinju <jinju201118@gmail.com>
2023-04-14 23:26:48 +0900

    variable

---
+++ lectures/3_javascript/variables.js
@@ -0,0 +1,17 @@
+// let teacher = '유진주';
+// console.log(teacher);
+// teacher = '이지호';
+// console.log(teacher);
+
+// const username = '유진주';
+// console.log(username);
+
+{
+  let a = '변수';
+  console.log('a: ', a);
+  {
+    let c = '변수';
+    console.log('c: ', c);
+    console.log('a: ', a);
+  }
+}
