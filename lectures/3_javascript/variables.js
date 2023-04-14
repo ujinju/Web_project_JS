7562ca7eab3220b8ea129053b771797134bd76ca
ujinju <jinju201118@gmail.com>
2023-04-14 23:41:14 +0900

    variable

--- lectures/3_javascript/variables.js
+++ lectures/3_javascript/variables.js
@@ -1,3 +1,7 @@
+'use strict';
+number = 3;
+console.log(number);
+
 // let teacher = '유진주';
 // console.log(teacher);
 // teacher = '이지호';
@@ -6,12 +10,25 @@
 // const username = '유진주';
 // console.log(username);
 
-{
-  let a = '변수';
-  console.log('a: ', a);
-  {
-    let c = '변수';
-    console.log('c: ', c);
-    console.log('a: ', a);
-  }
-}
+// {
+//   let a = '변수';
+//   console.log('a: ', a);
+//   {
+//     let a = '변수2';
+//     let c = '변수';
+//     console.log('c: ', c);
+//     console.log('a: ', a);
+//   }
+// }
+
+// function sum(a, b) {
+//   let result = a + b;
+//   return result;
+// }
+
+// console.log(sum(1, 2));
+// function sum(num1, num2) {
+//   return num1 + num2;
+// }
+
+//호이스팅은 var 변수나 function만 가능
