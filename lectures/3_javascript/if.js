802c4320cbfb8c8f879a78cbf2854ce2b2735cc2
ujinju <jinju201118@gmail.com>
2023-04-17 22:29:33 +0900

    if

---
+++ lectures/3_javascript/if.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// let number = 10;
+// if (number > 100) {
+//   console.log('참입니다.');
+// } else {
+//   console.log('거짓입니다.');
+// }
+// let score = prompt('수학 점수를 입력해주세요!');
+// console.log('score: ', score);
+// //90 이상 "등급은 A"
+// //80 이상 "등급은 B"
+// //70 이상 "등급은 C"
+// //60 이상 "등급은 D"
+// //나머지 다른값은 "등급은 F"
+// if (score >= 90) {
+//   console.log('등급은 A');
+// } else if (score >= 80) {
+//   console.log('등급은 B');
+// } else if (score >= 70) {
+//   console.log('등급은 C');
+// } else if (score >= 60) {
+//   console.log('등급은 D');
+// } else {
+//   console.log('등급은 F');
+// }
+
+let condition = NaN;
+if (condition) {
+  console.log('참입니다.');
+} else {
+  console.log('거짓입니다.');
+}
